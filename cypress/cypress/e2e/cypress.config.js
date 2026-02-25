865037faa109ccc4b2c61ee4fad419ff0187a3de
Gigio130 <earistizabal61@soy.sena.edu.co>
2026-02-25 17:42:38 -0500

    Se agrega gitignore y se limpian las dependencias

--- cypress/cypress/e2e/cypress.config.js
+++
@@ -1,9 +0,0 @@
-const { defineConfig } = require('cypress')
-
-module.exports = defineConfig({
-  e2e: {
-    // Aquí definimos la ruta correcta donde están las pruebas
-    specPattern: 'cypress/e2e/**/*.cy.js',
-    baseUrl: 'http://localhost/WebAbrigaalCampoV2', // Opcional, si quieres evitar repetir URLs
-  }
-})
